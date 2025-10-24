9a8f211a2def68679c8bdd321d7701b071d0be7c
Elevate-U <31393695+Elevate-U@users.noreply.github.com>
2025-10-24 06:09:42 +0000

    Deploying to gh-pages from @ Elevate-U/fylm@aa3acfd8e1b03b89948650df026a2f920eece072 🚀

---
+++ assets/SkeletonCard-HUJuCd8-.js
@@ -0,0 +1,2 @@
+import{u as e}from"./index-DJKKYvGv.js";import"./react-vendor-BYlzYzcd.js";const o=()=>e("div",{class:"skeleton-card",children:[e("div",{class:"skeleton-image"}),e("div",{class:"skeleton-text"}),e("div",{class:"skeleton-text short"})]});export{o as S};
+//# sourceMappingURL=SkeletonCard-HUJuCd8-.js.map
